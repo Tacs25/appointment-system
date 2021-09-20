8972856be43479d2bcae06f3e84fed3b3044022a
Tacs25 <adriantacdol2@gmail.com>
2021-09-20 15:28:32 +0800

    Remove button

--- main/footer.php
+++ main/footer.php
@@ -8,7 +8,6 @@
       </div>
       <!-- Footer -->
       <!--scroll top-->
-      <button id="topbtn"><i class="fas fa-arrow-circle-up"></i></button>
       <!--scroll top-->
 
 
@@ -18,6 +17,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
     
     <!-- Custom Js -->
-    <script src="js/script.js"  text="text/javascript"></script>
+    
 </body>
 </html>
